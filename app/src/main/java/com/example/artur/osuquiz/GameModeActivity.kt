b31b3ca3963bbaa5430326b8b8085bc7677b61c8
Arkitev <gora1025@gmail.com>
2019-01-26 03:26:26 +0100

    Activities Views done

---
+++ app/src/main/java/com/example/artur/osuquiz/GameModeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.artur.osuquiz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GameModeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_mode)
+    }
+}
